5535fe2568e5817f9b30ef020e42872de97f5ff3
fiona <fiona@mai.co.id>
2025-11-07 15:57:33 +0700

    upload project

---
+++ config.js
@@ -0,0 +1,10 @@
+const emailConfig = {
+  PUBLIC_KEY: '4df7jefmtFJbjUTSL',
+  SERVICE_ID: 'service_1wqd7zj',
+  TEMPLATE_ID: 'template_vcxrpd4',
+  RECAPTCHA_SITE_KEY: '6LeEhnsrAAAAACPSFVoODLMU1Dfh65pjuebgwiIT'
+<<<<<<< HEAD
+};
+=======
+};
+>>>>>>> 93cd3be (update portfolio and CV)
